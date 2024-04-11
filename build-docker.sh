4ca64fc8b34acb52bb7344bb767a817a9f07588a
Leonardo Amaral <contato@leonardoamaral.com.br>
2024-04-11 09:16:00 -0300

    Added --push to build-docker.sh
    Signed-off-by: Leonardo Amaral <contato@leonardoamaral.com.br>

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
 
 for branch in v2.8.2; do
-	docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v5,linux/arm/v6,linux/arm/v7 --tag quay.io/pqatsi/docker-nut:${branch} --tag quay.io/pqatsi/docker-nut:latest --build-arg branch=${branch} .
+	docker buildx build --push --platform linux/amd64,linux/arm64,linux/arm/v5,linux/arm/v6,linux/arm/v7 --tag quay.io/pqatsi/docker-nut:${branch} --tag quay.io/pqatsi/docker-nut:latest --build-arg branch=${branch} .
 done
