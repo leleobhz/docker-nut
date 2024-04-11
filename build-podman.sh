db29a8de57e9731177b82297bf8d2e96bd3c85c3
Leonardo Amaral <contato@leonardoamaral.com.br>
2024-04-11 08:50:30 -0300

    Build scripts for podman and docker
    Nut version 2.8.2
    
    Signed-off-by: Leonardo Amaral <contato@leonardoamaral.com.br>

--- build-podman.sh
+++ build-podman.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-for branch in v2.8.1; do
+for branch in v2.8.2; do
 	BUILD_FORMAT=docker podman build --platform linux/amd64,linux/arm64,linux/arm/v5,linux/arm/v6,linux/arm/v7 --manifest docker-nut --build-arg branch=${branch} .
 	BUILD_FORMAT=docker podman manifest push docker-nut docker://quay.io/pqatsi/docker-nut:${branch}
 	BUILD_FORMAT=docker podman manifest push docker-nut docker://quay.io/pqatsi/docker-nut:latest
